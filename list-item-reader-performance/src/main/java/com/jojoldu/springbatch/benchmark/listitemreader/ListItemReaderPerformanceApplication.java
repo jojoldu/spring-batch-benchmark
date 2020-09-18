adfda5c622e95387819536baad4f9d1e650a18e3
이동욱 <jojoldu@gmail.com>
2020-09-18 19:26:26 +0900

    ListItemReader 비교 진행

---
+++ list-item-reader-performance/src/main/java/com/jojoldu/springbatch/benchmark/listitemreader/ListItemReaderPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.springbatch.benchmark.listitemreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListItemReaderPerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListItemReaderPerformanceApplication.class, args);
+    }
+
+}
---
+++ list-item-reader-performance/src/test/java/com/jojoldu/springbatch/benchmark/listitemreader/ListItemReaderPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.springbatch.benchmark.listitemreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListItemReaderPerformanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
