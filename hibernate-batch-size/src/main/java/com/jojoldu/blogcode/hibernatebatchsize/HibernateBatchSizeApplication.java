1d4c9ebab66b91e3125e4c27bda07fd0c40329e6
이동욱 <jojoldu@gmail.com>
2021-01-28 09:14:13 +0900

    hibernate batch size 예제 코드 시작

---
+++ hibernate-batch-size/src/main/java/com/jojoldu/blogcode/hibernatebatchsize/HibernateBatchSizeApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.blogcode.hibernatebatchsize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateBatchSizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateBatchSizeApplication.class, args);
+    }
+
+}
---
+++ hibernate-batch-size/src/test/java/com/jojoldu/blogcode/hibernatebatchsize/HibernateBatchSizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.blogcode.hibernatebatchsize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateBatchSizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
